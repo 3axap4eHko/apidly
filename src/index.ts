6046ae60efdc19210e514049b0ed52033cb12195
3axap4eHko <3axap4eHko@gmail.com>
2021-03-27 16:23:31 -0400

    export Executor type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { Request, RequestMiddleware, RequestOptions, RequestParameters, Events, Value } from './Request';
+export { execute, Executor, ExecutorOptions, ExecutorParameters } from './Executor';
 export { createClient, Client, ClientOptions } from './client';
 export { createEndpoint, Endpoint, EndpointOptions } from './endpoint';
 export { createCredentials } from './credentials';
